5096d0d58b107d4cf93c727ce8eff45a9a7d8553
xenial <xenial@localhost.localdomain>
2020-12-13 04:26:39 +0100

    v fmt files

--- bstone/bstone.v
+++ bstone/bstone.v
@@ -2,21 +2,19 @@ module bstone
 
 pub struct Server {
 pub:
-    port int = 19132
-    number_of_players int = 100
-    name string
-
+	port              int = 19132
+	number_of_players int = 100
+	name              string
 pub mut:
-    players map[string]Player
+	players           map[string]Player
 }
 
 pub fn (mut s Server) start() {
-
 }
 
 pub fn (mut s Server) stop() {
 }
 
 pub fn (mut s Server) add_player(player Player) {
-    s.players[player.hash_code().str()] = player
+	s.players[player.hash_code().str()] = player
 }
--- bstone/player.v
+++ bstone/player.v
@@ -3,26 +3,24 @@ module bstone
 import vraklib
 
 struct Player {
-    vraklib vraklib.VRakLib
-
-    server Server
-    ip string
-    port u16
+	vraklib vraklib.VRakLib
+	server  Server
+	ip      string
+	port    u16
 }
 
 pub fn new_player(vraklib vraklib.VRakLib, server Server, ip string, port u16) Player {
-    return Player {
-        vraklib: vraklib
-        server: server
-        ip: ip
-        port: port
-    }
+	return Player{
+		vraklib: vraklib
+		server: server
+		ip: ip
+		port: port
+	}
 }
 
-fn (p Player) handle_data_packet(packet vraklib.Packet) {//TODO uhm.. shouldn't these be split?
-    
+fn (p Player) handle_data_packet(packet vraklib.Packet) { // TODO uhm.. shouldn't these be split?
 }
 
 fn (p Player) hash_code() int {
-    return 0
+	return 0
 }
