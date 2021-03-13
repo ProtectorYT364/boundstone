47c878157d98ed4102709924eb8601876d96ad39
XenialDan <thebigsmilexd@gmail.com>
2021-03-13 18:09:59 +0100

    MOTD fixed - wrong endianness all the time

--- pingtest.v
+++ pingtest.v
@@ -3,22 +3,31 @@ import net
 import time
 
 fn main() {
-	address := '127.0.0.1:19132'
+	address := '179.61.251.10:19132'
+	rsaddr, rport := net.split_address(address) or { panic(err) } // ?
+	rnet_addr := net.Addr{
+		saddr: rsaddr
+		port: rport
+	}
 
 	mut c := net.dial_udp('127.0.0.1:19133', address) or { panic(err) }
 	defer {
 		c.close() or { }
 	}
+	saddr, port := net.split_address('127.0.0.1:19133') or { panic(err) } // ?
+	net_addr := net.Addr{
+		saddr: saddr
+		port: port
+	}
 
-	mut fuckoff := []byte{len: 33}
-	mut b := vraklib.new_bytebuffer(fuckoff, u32(33))
-	mut ping := vraklib.UnConnectedPing{send_timestamp: vraklib.timestamp(), client_guid: u64(1)}
-	ping.encode(mut b)
+	mut b :=[]byte{len: 33}
+	mut ping := vraklib.UnConnectedPing{p:vraklib.new_packet(b, net_addr ),send_timestamp: vraklib.timestamp(), client_guid: u64(1)}
+	ping.encode(mut ping.p.buffer)
 	println('UnConnectedPing: "$ping"')
-	println('Encoded "$b.buffer.bytestr()"')
-	c.write(b.buffer) or { panic(err) }
+	println('Encoded "$ping.p.buffer"')
+	c.write(ping.p.buffer.buffer) or { panic(err) }
 	//rewind buffer
-	b.position = 0
+	ping.p.buffer.rewind()
 	//ping.decode(mut b)
 	//println('De-Encoded UnConnectedPing: "$ping"')
 	//println('De-Encoded "$b.buffer.bytestr()"')
@@ -26,23 +35,19 @@ fn main() {
 //	c.write(b.buffer) or { panic(err) }
 
 	mut data := []byte{len: 1492}
-	n, addr := c.read(mut data) or { panic(err) }
-	println('Got address $addr')
+	n, caddr := c.read(mut data) or { panic(err) }
+	println('Got address $caddr')
 	println('Got $n vs $data.len bytes: "$data.bytestr()"')
 	//trim data
 	data = data[..n]
 	println('Got $n bytes: "$data.bytestr()"')
 
 	//TODO read unconnected pong
-	b.position = 0
-	b.buffer = data
-	b.length = u32(b.buffer.len)
 	
-	pid := b.get_byte()
+	mut pong := vraklib.UnConnectedPong{p:vraklib.new_packet(data, rnet_addr )}
+	pid := pong.p.buffer.get_byte()
 	println(pid)
-	mut pong := vraklib.UnConnectedPong{}
-	println(pong)
-	pong.decode(mut b)
+	pong.decode(mut pong.p.buffer)
 	println(pong)
 	println(pong.data.bytestr())
 
